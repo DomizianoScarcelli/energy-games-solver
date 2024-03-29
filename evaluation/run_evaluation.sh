99d1134af3c7523dbf5ca646159e728d40f333b3
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2024-03-29 10:36:18 +0100

    Added arena generation evaluation results

--- evaluation/run_evaluation.sh
+++ evaluation/run_evaluation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Check if the first argument is 'skip'
-if [ "$1" != "skip-generate" ]; then
-    # Generate the arenas
-    python -m run --generate --node-space 10 50 100 200 500 1000 5000 10000 --probability-space 0.01 0.05 0.1 0.2 0.5
-fi
+# # Check if the first argument is 'skip'
+# if [ "$1" != "skip-generate" ]; then
+#     # Generate the arenas
+#     python -m run --generate --node-space 10 50 100 200 500 1000 5000 10000 --probability-space 0.01 0.05 0.1 0.2 0.5
+# fi
 
 # Evaluate the algorithms on all the generated arenas
 for file in arenas/*.pkl; do
